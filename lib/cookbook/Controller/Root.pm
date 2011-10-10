34ee37f4a4bef7ad5c73875a3104ef78855ac098
Daniil Popov <popov.daniil@gmail.com>
2011-10-11 02:21:06 +0400

    add template for index

--- lib/cookbook/Controller/Root.pm
+++ lib/cookbook/Controller/Root.pm
@@ -30,7 +30,8 @@ sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
     # Hello World
-    $c->response->body( $c->welcome_message );
+    #$c->response->body( $c->welcome_message );
+    $c->stash(message=> 'helloooooo!');
 }
 
 =head2 default
---
+++ t/view_TT.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::More;
+
+BEGIN { use_ok 'cookbook::View::TT' }
+
+done_testing();
