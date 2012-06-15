93fd57b17c297de6631437f81329773d70fe55f7
celebithil <popov.daniil@gmail.com>
2012-06-15 16:02:56 +0400

    add new controller

---
+++ lib/Cookbook/Controller/Typec.pm
@@ -0,0 +1,44 @@
+package Cookbook::Controller::Typec;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Cookbook::Controller::Typec - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Cookbook::Controller::Typec in Typec.');
+}
+
+
+=head1 AUTHOR
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
