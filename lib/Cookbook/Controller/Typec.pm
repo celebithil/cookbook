d5a0016ea364f17dff00757141d33003e359b0b9
Daniil Popov <popov.daniil@gmail.com>
2012-06-17 14:50:25 +0400

    add test controller for chained

--- lib/Cookbook/Controller/Typec.pm
+++ lib/Cookbook/Controller/Typec.pm
@@ -21,11 +21,46 @@ Catalyst Controller.
 
 =cut
 
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
-
-    $c->response->body('Matched Cookbook::Controller::Typec in Typec.');
+sub base :Chained('/') :PathPart('typec') :CaptureArgs(0) {
+    my ( $self,  $c ) = @_;  
+    
 }
+
+sub list :Chained('base') :PathPart('') :Args(0) {
+    my ( $self,  $c ) = @_;
+    my $rs = $c->model('CookbookDB::Type')->search(
+            {},
+            {
+                columns  => [qw /type_name type_id/],
+                order_by => 'type_name'
+            }
+    );
+   $c->stash(
+        types => $rs,
+        template => 'type/list.tt',
+    );
+}
+
+
+sub add :Chained('base') :PathPart('add') :Args(0) {
+	    my ( $self,  $c ) = @_;
+	    $c->stash( template => 'type/add.tt',);
+	      
+}
+
+sub edit :Chained('base') :PathPart('edit') :Args(1) {
+	    my ( $self,  $c ) = @_;	      
+}
+
+sub delete :Chained('base') :PathPart('delete') :Args(1) {
+	    my ( $self,  $c ) = @_;	      
+}
+
+sub view :Chained('base') :PathPart('view') :Args(1) {
+	    my ( $self,  $c ) = @_;	      
+}
+
+
 
 
 =head1 AUTHOR
