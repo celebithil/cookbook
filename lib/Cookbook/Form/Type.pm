66a2856b0eba54c72e0bb1d469d9f08f4a217bb7
celebithil <popov.daniil@gmail.com>
2012-06-26 15:03:11 +0400

    save result

--- lib/Cookbook/Form/Type.pm
+++ lib/Cookbook/Form/Type.pm
@@ -4,10 +4,18 @@ use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler::Model::DBIC';
 use namespace::autoclean;
 
-has '+item_class' => ( default =>'Type' );
-has_field 'type_name' => (type => 'Text', required => 1);
-has_field 'submit' => ( type => 'Submit', value => 'Ввести' );
-has_field 'cancel' => ( type => 'Submit', value => 'Отменить' );
+has '+item_class' => (default =>'Type',);
+
+has_field 'type_name' => (
+    type => 'Text',
+    required => 1,
+    label => 'Type',
+);
+
+has_field 'submit' => (
+    type => 'Submit',
+    value => 'Ввести',
+);
 
 __PACKAGE__->meta->make_immutable;
 1;
